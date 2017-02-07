85c97b5f93bbd2142f4320f7c145451bbc2f94fb
Hilary Lewis <hilarylewis92@gmail.com>
2017-02-06 18:44:00 -0700

    Add home text

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 
 import Home from './Home'
+import Nav from './Nav'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Nav />
         <Home />
       </div>
     )
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,42 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import { TweenLite, Linear } from 'gsap'
 
 class Home extends Component {
+  componentDidMount() {
+    this.gsapHeroAnimation()
+  }
+
+  gsapHeroAnimation() {
+    TweenLite.to('.hero-text', 1,
+      {
+        top: 200,
+        opacity: 1,
+        lazy: true,
+      }
+    )
+  }
+
   render() {
     return (
-      <div className="Home">
-        <h3>Hilary Lewis.</h3>
-        <h3>Mathematical.</h3>
-        <h3>Creative.</h3>
-        <h3>Teacher.</h3>
-        <h3>Learner.</h3>
-        <h3
-          className='bold'>
-          Front End Developer.
-        </h3>
-        <h3> Denver, Colorado.</h3>
+      <div className='Home'>
+        <section className='hero-text'>
+          <h3>
+            Hilary Lewis.
+          </h3>
+          <h3>
+            Mathematical. Creative.
+          </h3>
+          <h3>
+            Teacher. Learner.
+          </h3>
+          <h3
+            className='bold'>
+            Front End Developer.
+          </h3>
+          <h3>
+            Denver, Colorado.
+          </h3>
+        </section>
       </div>
     )
   }
