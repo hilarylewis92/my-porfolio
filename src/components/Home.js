0c1ebbfe0a106d544b7a9af099ba089497b24dc8
Hilary Lewis <hilarylewis92@gmail.com>
2017-02-06 17:56:43 -0700

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <h3>Hilary Lewis.</h3>
+        <h3>Mathematical.</h3>
+        <h3>Creative.</h3>
+        <h3>Teacher.</h3>
+        <h3>Learner.</h3>
+        <h3
+          className='bold'>
+          Front End Developer.
+        </h3>
+        <h3> Denver, Colorado.</h3>
+      </div>
+    )
+  }
+}
+
+export default Home
